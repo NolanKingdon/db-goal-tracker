c65a3624e8fce6af4abd026cd3e6fa39dbc830b5
Nolan Kingdon <nolan.kingdon@gmail.com>
2018-06-30 16:08:27 -0400

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
-
 import { firebaseApp } from './firebase';
+import reducer from './reducers';
+import { logUser } from './actions';
 
 import App from './components/app';
 import SignUp from './components/signUp';
 import SignIn from './components/signIn';
 
+const store = createStore(reducer);
 
 firebaseApp.auth().onAuthStateChanged(user => {
   if(user) {
+    const { email } = user;
+    store.dispatch(logUser(email));
     browserHistory.push("/app");
   } else {
     browserHistory.replace("/signIn");
@@ -18,9 +24,11 @@ firebaseApp.auth().onAuthStateChanged(user => {
 })
 
 ReactDOM.render(
-  <Router path = "/" history = { browserHistory }>
-      <Route path = "/app" component = {App} />
-      <Route path = "/signin" component = {SignIn} />
-      <Route path = "/signup" component = {SignUp} />
-  </Router>, document.getElementById('root')
+  <Provider store = { store }>
+    <Router path = "/" history = { browserHistory }>
+        <Route path = "/app" component = {App} />
+        <Route path = "/signin" component = {SignIn} />
+        <Route path = "/signup" component = {SignUp} />
+    </Router>
+  </Provider>, document.getElementById('root')
 )
